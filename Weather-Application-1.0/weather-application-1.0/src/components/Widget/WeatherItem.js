80cbdfd7cae3e6a245c95d139297b29fec343cb3
zhennanunc <matthewfznn@gmail.com>
2022-10-16 22:15:36 -0400

    Icon_Updated

--- Weather-Application-1.0/weather-application-1.0/src/components/Widget/WeatherItem.js
+++ Weather-Application-1.0/weather-application-1.0/src/components/Widget/WeatherItem.js
@@ -1,5 +1,22 @@
 import Card from "../../UI/Card";
 import "./WeatherItem.css";
+import "../../weather-icons-master/css/weather-icons.css";
+
+const updateWeatherStatusIcon = (props) => {
+  var status = props.status.toString().toLowerCase();
+  switch (status) {
+    case "clear":
+      return "wi wi-day-sunny";
+    case "clouds":
+      return "wi wi-cloud";
+    case "rain":
+      return "wi wi-rain";
+    case "smoke":
+      return "wi wi-smoke";
+    default: 
+      return "wi wi-na";
+  }
+};
 
 const WeatherItem = (props) => {
   return (
@@ -12,6 +29,9 @@ const WeatherItem = (props) => {
         <div className="condition">
           <div className="temp">{Math.round(props.temp)}°C</div>
           <div className="status">{props.status}</div>
+          <div className="icon">
+            <i class={updateWeatherStatusIcon(props)} />
+          </div>
         </div>
       </Card>
     </div>
